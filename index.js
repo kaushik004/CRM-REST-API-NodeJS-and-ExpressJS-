30d27aa7a7d63f0ce5b354003190268bf47bc4b0
kaushik <kaushik@gmail.com>
2021-01-17 01:16:43 +0530

    Created Initial routes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes'
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+routes(app);
 
 app.get('/', (req, res) => {
     res.send(`Node and Express server is running on port ${PORT}`);
